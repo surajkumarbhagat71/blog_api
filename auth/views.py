48ce0aad2abfb331379d64562c1c98b6f96beb25
suraj kumar <67819248+surajkumarbhagat71@users.noreply.github.com>
2023-01-24 16:18:59 +0530

    Update views.py

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#
+# class GetLoginToken(APIView):
+#     def post(self, request):
+#         user = User.objects.filter(username=request.data.get("username")).first()
+#         password = request.data.get("password")
+#         user_type = None
+#         try:
+#             user_type = user.user_type.name.upper()
+#         except:
+#             pass
+#
+#         if user is not None and user.check_password(password):
+#             refresh = RefreshToken.for_user(user)
+#             context = {
+#                 'refresh': str(refresh),
+#                 'access': str(refresh.access_token),
+#                 'user_type':user_type,
+#             }
+#             return Response(context, status.HTTP_200_OK)
+#         else:
+#             context = {
+#                 "detail": "No active account found with the given credentials"
+#             }
+#             return Response(context,status.HTTP_401_UNAUTHORIZED)
+
